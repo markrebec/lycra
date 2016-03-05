28a07dbb39cdc66f8ea7e6e2e393f8bc737ac19b
Mark Rebec <mark@markrebec.com>
2016-03-05 13:04:57 -0800

    adds specs for Lycra.logger config

---
+++ spec/lycra_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+RSpec.describe Lycra do
+  describe '.logger' do
+    context 'when configured with a custom logger' do
+      let(:logger) do
+        Dir.mktmpdir do |dir|
+          Logger.new(File.open(File.join(dir, 'lycra.log'), 'w'))
+        end
+      end
+
+      before do
+        Lycra.configuration.configure(logger: logger)
+        Lycra.instance_variable_set(:@logger, nil)
+      end
+
+      it 'uses the custom logger' do
+        expect(Lycra.logger).to eq(logger)
+      end
+    end
+
+    context 'with default configuration' do
+      before do
+        Lycra.configuration.configure(logger: nil)
+        Lycra.instance_variable_set(:@logger, nil)
+      end
+
+      it 'is an instance of Logger' do
+        expect(Lycra.logger).to be_an_instance_of(::Logger)
+      end
+
+      it 'uses STDOUT' do
+        expect(Lycra.logger.instance_variable_get(:@logdev).dev).to eq(STDOUT)
+      end
+    end
+  end
+end
