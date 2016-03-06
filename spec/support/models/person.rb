6bf2a127ced4320e0a5d2b1b30508d02ae10bdbb
Mark Rebec <mark@markrebec.com>
2016-03-06 12:32:43 -0800

    custom import method for spec person model

--- spec/support/models/person.rb
+++ spec/support/models/person.rb
@@ -15,4 +15,33 @@ class Person
       email: email,
       age: age }
   end
+
+  # TODO write a SpecModelAdapter for ActiveModel that implements __transform and __find_in_batches instead
+  def self.import(records, options={})
+    errors = []
+    target_index = options.delete(:index) || index_name
+    target_type = options.delete(:type) || document_type
+
+    if options.delete(:force)
+      __elasticsearch__.create_index! force: true, index: target_index
+    elsif !__elasticsearch__.index_exists? index: target_index
+      raise ArgumentError,
+        "#{target_index} does not exist to be imported into. Use create_index! or the :force option to create it."
+    end
+
+    records.each_slice(2_000) do |recs|
+      response = __elasticsearch__.client.bulk \
+        index: target_index,
+        type: target_type,
+        body: recs.map { |rec| { index: { _id: rec.id, data: rec.__elasticsearch__.as_indexed_json } } }
+
+      yield response if block_given?
+
+      errors +=  response['items'].select { |k, v| k.values.first['error'] }
+    end
+
+    __elasticsearch__.refresh_index! if options.delete(:refresh)
+
+    errors
+  end
 end
