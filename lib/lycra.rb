26e16107045b0c2f5eb68c72587b7bf2fb56508c
Mark Rebec <mark@markrebec.com>
2016-09-20 17:57:15 -0700

    adds a few more base config options

--- lib/lycra.rb
+++ lib/lycra.rb
@@ -10,7 +10,11 @@ module Lycra
     config.elasticsearch_host = ENV['ELASTICSEARCH_HOST'] || 'localhost'    # elasticsearch host to use when connecting, defaults to ENV var if set or falls back to localhost
     config.elasticsearch_port = ENV['ELASTICSEARCH_PORT'] || 9200           # elasticsearch port to use when connecting, defaults to ENV var if set or falls back to 9200
     config.elasticsearch_url  = ENV['ELASTICSEARCH_URL']                    # elasticsearch URL to use when connecting (i.e. 'https://localhost:9200'), this will override host/port if set
-    config.logger             = nil                                         # defaults to STDOUT but will use Rails.logger in a rails environment (via Lycra::Engine)
+    config.index_prefix       = nil                                         # a prefix to use for index names (i.e. 'my-app' prefix and 'people' index becomes 'my-app-people')
+    config.log                = false                                       # whether or not the elasticsearch client should perform standard logging
+    config.logger             = nil                                         # logger use for standard elasticsearch logging, defaults to STDOUT but will use Rails.logger in a rails environment (via Lycra::Engine)
+    config.trace              = false                                       # whether or not the elasticsearch client should log request/response traces
+    config.tracer             = nil                                         # logger used when tracing request/response data
 
     def elasticsearch_url
       @state[:elasticsearch_url] || "#{self.elasticsearch_host}:#{self.elasticsearch_port}"
@@ -19,6 +23,16 @@ module Lycra
     def logger
       @logger ||= (@state[:logger] || Logger.new(STDOUT))
     end
+  end
+
+  def self.client
+    @client ||= Elasticsearch::Client.new(
+      host: configuration.elasticsearch_url,
+      log: configuration.log,
+      logger: configuration.logger,
+      trace: configuration.trace,
+      tracer: configuration.tracer
+    )
   end
 
 end
