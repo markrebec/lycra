d790057bb162bf4392843d2a9b30150016bd7270
Mark Rebec <mark@markrebec.com>
2018-08-25 14:26:24 -0700

    more examples

--- app/documents/vehicle_document.rb
+++ app/documents/vehicle_document.rb
@@ -1,8 +1,11 @@
 class VehicleDocument < Lycra::Document::Base
-  attribute :slug, String
-  attribute :name, String
-  attribute :description, Lycra::Text
-  attribute :summary, Lycra::Text do
+  attribute :slug,        String,       :slug,  'A unique slug'
+  attribute :name,        String,               'The name of the vehicle'
+  attribute :description, Lycra::Text,          'A description of the vehicle'
+
+  attribute :summary,     Lycra::Text do
+    description 'A concatenated summary for searching'
+
     resolve ->(obj, arg, ctx) do
       "#{obj.name} #{obj.description}"
     end
