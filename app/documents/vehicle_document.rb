7ad3754df1caee707ebf2fda13b97e33fe350c98
Mark Rebec <mark@markrebec.com>
2018-08-25 12:14:43 -0700

    basic test document for vehicles

--- app/documents/vehicle_document.rb
+++ app/documents/vehicle_document.rb
@@ -1,2 +1,10 @@
 class VehicleDocument < Lycra::Document::Base
+  attribute :slug, String
+  attribute :name, String
+  attribute :description, Lycra::Text
+  attribute :summary, Lycra::Text do
+    resolve ->(obj, arg, ctx) do
+      "#{obj.name} #{obj.description}"
+    end
+  end
 end
